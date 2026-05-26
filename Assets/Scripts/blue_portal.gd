e4ffa7264189dc36a714a015213483135a7a8d70
Dylan-2-tech <dylanhoungbedji@gmail.com>
2026-05-26 17:50:58 +0200

    Téléportation en fonction de l'orientation

--- Assets/Scripts/blue_portal.gd
+++ Assets/Scripts/blue_portal.gd
@@ -1,12 +1,21 @@
 extends Area2D
 
-var linked_portal: Area2D  # no @export, set from outside
+var linked_portal: Area2D
 var is_teleporting = false
 
 func _on_body_entered(body):
 	if body is PlayerController and !is_teleporting:
 		linked_portal.is_teleporting = true
+		
+		# Convert velocity from entry portal local space to exit portal local space
+		var local_velocity = to_local(body.global_position + body.velocity) - to_local(body.global_position)
+		var rotated_velocity = linked_portal.to_global(local_velocity) - linked_portal.global_position
+
+		# Teleport the player
 		body.global_position = linked_portal.global_position
+		
+		# Apply the remapped velocity
+		body.velocity = rotated_velocity
 
 func _on_body_exited(body):
 	if body is PlayerController:
